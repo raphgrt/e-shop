3d03c8e6868b1eeaa7ee0098e2e84ef089be5c57
Raphael GEORGET <raphael.georgetpro@orange.fr>
2024-09-05 08:19:02 +0200

    Add Assets and follow tutorial

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Options} from "../../types";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
+}
+
+
---
+++ src/app/services/products.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from "./api.service";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private apiService: ApiService ) { }
+
+  getProducts = (url: string, params: any): Observable<any> => {
+    return this.apiService.get(url, params);
+  }
+}
